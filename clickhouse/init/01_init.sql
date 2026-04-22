35f553fdb844995679526700be431e7a4ea45462
ilyapanov123 <89517673116g@gmail.com>
2026-04-22 21:12:20 +0300

    DATA UPD

--- clickhouse/init/01_init.sql
+++ clickhouse/init/01_init.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS logsantos.metrics
 (
     metricName String,
     value UInt64,
-    windowStart Decimal(20, 9),
-    windowEnd Decimal(20, 9),
-    createdAt Decimal(20, 9)
+    windowStart DateTime64(3, 'UTC'),
+    windowEnd DateTime64(3, 'UTC'),
+    createdAt DateTime64(3, 'UTC')
     )
     ENGINE = MergeTree
     ORDER BY (metricName, createdAt);
@@ -33,7 +33,7 @@ AS
 SELECT
     metricName,
     value,
-    windowStart,
-    windowEnd,
-    createdAt
+    toDateTime64(windowStart, 3, 'UTC') AS windowStart,
+    toDateTime64(windowEnd, 3, 'UTC') AS windowEnd,
+    toDateTime64(createdAt, 3, 'UTC') AS createdAt
 FROM logsantos.metrics_kafka;
